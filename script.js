0ab35ece394ca3f2b60d3d9003fa1b1ddc2f13f0
Janardhan025 <janardhanm8897@gmail.com>
2024-12-20 18:25:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function LightOn(){
+    let img=document.getElementById("image")
+    img.src="bulb-onn.png"
+    img.innerHTML= "Result :" +img
+}
+function LightOff(){
+    let img=document.getElementById("image")
+    img.src="bulb-off.png"
+    img.innerHTML= "Result :" +img
+}
